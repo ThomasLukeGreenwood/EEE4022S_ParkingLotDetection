ba7a6151be32345a102a7d6861987a98e33f5bc1
thomas Greenwood <GRNTHO006@myuct.ac.za>
2024-10-29 16:23:12 +0200

    cleaning code

--- DemoDayCode/Flask_app/app.py
+++ DemoDayCode/Flask_app/app.py
@@ -9,39 +9,11 @@ app = Flask(__name__)
 #director of the home route
 def update_number():
     global number
-    model = YOLO("best.pt")
-    #Capture the image
-    #cap = cv2.VideoCapture(0) # For edge based processors use own webcam
-    URL = "http://192.168.4.1" # For cloud based connect to url and get the data from here
-    cap = cv2.VideoCapture(URL + ":81/stream") #/81stream
-    result, frame = cap.read()
-    results = model(frame,
-    conf = 0.07, 
-    verbose = False)
-    cap.release()
-    number = results[0].boxes.cls.tolist().count("empty")
-    names = model.names
-    car_id = list(names)[list(names.values()).index('empty')]
-    number = results[0].boxes.cls.tolist().count(car_id)
-    index()
+    number = makePredictions()
     return jsonify(number=number)
 @app.route('/')
 def index():
-
-    model = YOLO("best.pt")
-    #Capture the image
-    #cap = cv2.VideoCapture(0) # For edge based processors use own webcam
-    URL = "http://192.168.4.1" # For cloud based connect to url and get the data from here
-    cap = cv2.VideoCapture(URL + ":81/stream") #/81stream
-    result, frame = cap.read()
-    results = model(frame,
-    conf = 0.07, 
-    verbose = False)
-    cap.release()
-    number = results[0].boxes.cls.tolist().count("empty")
-    names = model.names
-    car_id = list(names)[list(names.values()).index('empty')]
-    number = results[0].boxes.cls.tolist().count(car_id)
+    number = makePredictions()
     return render_template('index.html',number = number)
 @app.route('/update_number')
 
@@ -50,7 +22,10 @@ def index():
 @app.route('/dynamic')
 def dynamic():
     #Load model
-    
+    makePredictions()
+    return render_template('dynamic.html')
+
+def makePredictions():
     model = YOLO("best.pt")
     #Capture the image
     #cap = cv2.VideoCapture(0) # For edge based processors use own webcam
@@ -58,16 +33,20 @@ def dynamic():
     cap = cv2.VideoCapture(URL + ":81/stream") #/81stream
     result, frame = cap.read()
     results = model(frame,
-    conf = 0.07, 
-    verbose = False)
+    conf = 0.15,
+    iou = 0.8, 
+    verbose = False,
+    show_labels = False)
     annotated_frame = results[0].plot()
     cv2.imwrite(
         "Flask_app/static/images/dynamic_image.jpg",
         annotated_frame)
-    # Break the loop if 'q' is pressed
     cap.release()
-    return render_template('dynamic.html')
-
-
+    number = results[0].boxes.cls.tolist().count("empty")
+    names = model.names
+    car_id = list(names)[list(names.values()).index('empty')]
+    number = results[0].boxes.cls.tolist().count(car_id)
+    return number
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
